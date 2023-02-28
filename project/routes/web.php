770dc6514b973405e19dfe900463ee9524c5f2a8
gileduardo <gilgea@gmail.com>
2023-02-28 18:40:52 -0300

    add project laravel 9

---
+++ project/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('welcome');
+    return "<h1>Gil</h1>";
+});
+
+Route::get('/docker', function () {
+    return "<h1>Docker Teste</h1>";
+});
