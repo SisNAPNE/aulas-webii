8c3b7fa8cb736029db7c7905e0aa726872e155ad
gileduardo <gilgea@gmail.com>
2023-11-22 16:13:33 -0300

    HTTP Client - OK

---
+++ Client-Http/app/Http/Controllers/HTTPClientController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class HTTPClientController extends Controller
+{
+    public function getApiData() {
+
+        $response = Http::get('https://randomuser.me/api/');
+        return $response->body();
+    }
+}
